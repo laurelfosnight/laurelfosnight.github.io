08ce4c52d97672c05af6ad7385b50626b46708a1
laurelfosnight <laurel.fosnight@gmail.com>
2016-11-29 16:13:58 -0800

    final changes

--- Fall16/Final/ThankYou/js/main.js
+++ Fall16/Final/ThankYou/js/main.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function () {
+	$('#nav-menu-btn').on('click', function () {
+		var $menuBtn = $(this),
+		    menuExpanded = $menuBtn.attr('aria-expanded'),
+		    $navMenu = $('#nav-menu');
+		    
+		if (menuExpanded === 'true') {
+			$menuBtn.attr('aria-expanded','false').removeClass('expanded');
+			$navMenu.removeClass('expanded');
+		} else {
+			$menuBtn.attr('aria-expanded','true').addClass('expanded');
+			$navMenu.addClass('expanded');
+		}
+	});
+});
